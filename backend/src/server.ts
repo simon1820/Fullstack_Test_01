90a43605c827069dc87c6e70947e851dd4a975b9
SIMON ANDRES GUIJARRO MURRIETA <simon.guijarro@iroute.local>
2025-11-16 00:22:38 -0500

    fix: proper api routes import

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ app.use(express.static(publicPath));
 
 app.use("/api", apiRoutes);
 
-app.get("*", (req, res) => {
+app.all("/*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
