87a6ad39e059be11a63c66f0e2ed61893b473894
SIMON ANDRES GUIJARRO MURRIETA <simon.guijarro@iroute.local>
2025-11-15 23:55:49 -0500

    feat: monolito listo para deploy v8

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ const publicPath = path.join(__dirname, "public");
 
 app.use(express.static(publicPath));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
 });
 
